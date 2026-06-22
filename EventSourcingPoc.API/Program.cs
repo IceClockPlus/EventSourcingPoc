5dfe7ad8053e2027ed46fb757f1c2785af4baba0
Alexis Aravena <aravenapro98@gmail.com>
2026-06-22 16:08:54 -0400

    Agregado libreria Scalar para documentacion de API

--- EventSourcingPoc.API/Program.cs
+++ EventSourcingPoc.API/Program.cs
@@ -1,6 +1,7 @@
 using EventSourcingPoc.API.Handlers;
 using Marten;
 using Marten.Events.Aggregation;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
