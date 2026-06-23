3740213630fc9ec27b9dcdb5ce2bde44bb90f46a
Alexis Aravena <aaravena@inge.cl>
2026-06-23 11:53:18 -0400

    Agregar ef para tabla de configuracion

---
+++ EventSourcingPoc.API/EFContext/GuaranteeContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+namespace EventSourcingPoc.API.EFContext
+{
+    public class GuaranteeContext : DbContext
+    {
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<Bond> Bonds { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public class Insurance
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public int? LegacyId { get; set; }
+        public decimal EnterpriseFactor { get; set; }
+        public decimal ExecutiveFactor { get; set; }
+        public decimal ExecutiveRenewalFactor { get; set; }
+    }
+
+    public class Bond
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int? LegacyId { get; set; }
+    }
+
+    public class BondConfiguration : IEntityTypeConfiguration<Bond>
+    {
+        public void Configure(EntityTypeBuilder<Bond> builder)
+        {
+            builder.HasKey(p => p.Id);
+        }
+    }
+
+    public class InsuranceConfiguration : IEntityTypeConfiguration<Insurance>
+    {
+        public void Configure(EntityTypeBuilder<Insurance> builder)
+        {
+            builder.HasKey(p => p.Id);
+        }
+    }
+}
--- EventSourcingPoc.API/Program.cs
+++ EventSourcingPoc.API/Program.cs
@@ -1,7 +1,10 @@
+using EventSourcingPoc.API.EFContext;
 using EventSourcingPoc.API.Handlers;
 using EventSourcingPoc.API.Projections;
 using Marten;
 using Marten.Events.Aggregation;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +22,15 @@ builder.Services.AddMarten(options =>
     options.Connection(builder.Configuration.GetConnectionString("Marten") ?? throw new ArgumentNullException());
     options.Projections.Add<GuaranteeClientProjection>(JasperFx.Events.Projections.ProjectionLifecycle.Async);
 }).AddAsyncDaemon(JasperFx.Events.Daemon.DaemonMode.HotCold);
+
+builder.Services.AddDbContext<GuaranteeContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Marten"), builder =>
+    {
+        builder.MigrationsAssembly(typeof(GuaranteeContext).Assembly.FullName);
+    });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
