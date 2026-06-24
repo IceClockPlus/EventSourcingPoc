5ea74b4ed6998ff31b28525d79ecbe9de1ad9d33
Alexis Aravena <aaravena@inge.cl>
2026-06-24 16:22:29 -0400

    Encapsular migraciones en una sola

---
+++ EventSourcingPoc.API/EFContext/Migrations/20260624202106_InitialMigration.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EventSourcingPoc.API.EFContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "bonds",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    legacy_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bonds", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "brokers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_brokers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "insurances",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    legacy_id = table.Column<int>(type: "integer", nullable: true),
+                    enterprise_factor = table.Column<decimal>(type: "numeric", nullable: false),
+                    executive_factor = table.Column<decimal>(type: "numeric", nullable: false),
+                    executive_renewal_factor = table.Column<decimal>(type: "numeric", nullable: false),
+                    certificate_number_counter = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_insurances", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bonds");
+
+            migrationBuilder.DropTable(
+                name: "brokers");
+
+            migrationBuilder.DropTable(
+                name: "insurances");
+        }
+    }
+}
