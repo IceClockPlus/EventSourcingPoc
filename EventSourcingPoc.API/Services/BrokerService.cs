7c6dd1e349b5d83686db65b308a66223c910c9ee
Alexis Aravena <aaravena@inge.cl>
2026-06-24 15:29:10 -0400

    Manejo de corredoras y aseguradoras

---
+++ EventSourcingPoc.API/Services/BrokerService.cs
@@ -0,0 +1,44 @@
+﻿using EventSourcingPoc.API.EFContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSourcingPoc.API.Services
+{
+    public class BrokerInfo
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+
+    public interface IBrokerService
+    {
+        Task<BrokerInfo?> GetBrokerInfoAsync(int id, CancellationToken cancellationToken);
+        Task<IReadOnlyList<BrokerInfo>> GetBrokerInfoAsync(CancellationToken cancellationToken);
+    }
+
+    public class BrokerService(GuaranteeContext context) : IBrokerService
+    {
+        private readonly GuaranteeContext _context = context;
+        public async Task<BrokerInfo?> GetBrokerInfoAsync(int id, CancellationToken cancellationToken)
+        {
+            var broker = await _context.Brokers.AsNoTracking()
+                .Select(b => new BrokerInfo
+                {
+                    Id = b.Id,
+                    Name = b.Name
+                }).FirstOrDefaultAsync(b => b.Id == id, cancellationToken);
+            return broker;
+        }
+
+        public async Task<IReadOnlyList<BrokerInfo>> GetBrokerInfoAsync(CancellationToken cancellationToken)
+        {
+            var brokers = await _context.Brokers
+                .AsNoTracking()
+                .Select(b => new BrokerInfo
+                {
+                    Id = b.Id,
+                    Name = b.Name
+                }).ToListAsync(cancellationToken);
+            return brokers;
+        }
+    }
+}
---
+++ EventSourcingPoc.API/Services/InsuranceService.cs
@@ -0,0 +1,78 @@
+﻿using EventSourcingPoc.API.EFContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSourcingPoc.API.Services
+{
+    public class InsuranceInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int? LegacyId { get; set; }
+        public long CurrentNumberCounter { get; set; }
+    }
+
+    public interface IInsuranceService
+    {
+        Task<InsuranceInfo?> GetInsurance(string id, CancellationToken cancellationToken);
+        Task<InsuranceInfo?> GetInsuranceByLegacyId(int legacyId, CancellationToken cancellationToken);
+        Task<long> GenerateNewCertificateNumber(string id, CancellationToken cancellationToken);
+    }
+
+    public class InsuranceService(GuaranteeContext context) : IInsuranceService
+    {
+        private readonly GuaranteeContext _context = context;
+        public async Task<InsuranceInfo?> GetInsurance(string id, CancellationToken cancellationToken)
+        {
+            var insurance = await _context.Insurances
+                .Select(i => new InsuranceInfo
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    LegacyId = i.LegacyId,
+                    CurrentNumberCounter = i.CertificateNumberCounter
+                }).FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+            return insurance;            
+        }
+
+        public async Task<InsuranceInfo?> GetInsuranceByLegacyId(int legacyId, CancellationToken cancellationToken)
+        {
+            var insurance = await _context.Insurances
+                .Select(i => new InsuranceInfo
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    LegacyId = i.LegacyId,
+                    CurrentNumberCounter = i.CertificateNumberCounter
+                }).FirstOrDefaultAsync(i => i.LegacyId == legacyId, cancellationToken);
+            return insurance;
+
+        }
+
+        /// <summary>
+        /// Increment and get the guarantee certificate number correlative for a specific insurance
+        /// </summary>
+        /// <param name="id">Insurance Id</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>New certificate number correlative</returns>
+        /// <remarks>
+        /// KEEP THIS IMPLEMENTATION
+        /// Make use of 'UPDATE .. RETURNING' to ensure atomicity of the operation.
+        /// The database locks the row, increment and return the 'certificate_number_counter' in a single indivisible step.
+        /// DOT NOT REFACTORING to either EF CORE (Read -> Update -> Save) nor 'ExecuteUpdateAsync' with a subsequent query,
+        /// since both will leave a milliseconds gap prone to race conditions on high concurrent environment
+        /// </remarks>
+        public async Task<long> GenerateNewCertificateNumber(string id, CancellationToken cancellationToken)
+        {
+            var certificateNumber = await _context.Database
+                .SqlQueryRaw<long>(
+                    @"UPDATE insurances
+                    SET certificate_number_counter = certificate_number_counter + 1
+                    WHERE id = {0}
+                    RETURNING certificate_number_counter",
+                    id
+                ).SingleAsync(cancellationToken);
+
+            return certificateNumber;
+        }
+    }
+}
