c3746ff4e01784598a48186091653ebc835c476e
Alexis Aravena <aaravena@inge.cl>
2026-06-26 15:18:25 -0400

    Nuevos endpoints de garantias para modificar datos y emitir

--- EventSourcingPoc.API/Controllers/GuaranteesController.cs
+++ EventSourcingPoc.API/Controllers/GuaranteesController.cs
@@ -8,14 +8,27 @@ namespace EventSourcingPoc.API.Controllers
     [ApiController]
     public class GuaranteesController : ControllerBase
     {
+        private readonly TimeProvider _timeProvider;
         private readonly ILogger<GuaranteesController> _logger;
         private readonly CreateGuaranteeHandler _createGuaranteeHandler;
         private readonly ConfirmGuaranteePriceHandler _confirmGuaranteePriceHandler;
-        public GuaranteesController(CreateGuaranteeHandler createGuaranteeHandler, ConfirmGuaranteePriceHandler confirmGuaranteePriceHandler, ILogger<GuaranteesController> logger)
+        private readonly UpdateGuaranteeInformationHandler _updateGuaranteeInformationHandler;
+        private readonly IssueGuaranteeHandler _issueGuaranteeHandler;
+
+        public GuaranteesController(
+            CreateGuaranteeHandler createGuaranteeHandler, 
+            ConfirmGuaranteePriceHandler confirmGuaranteePriceHandler, 
+            UpdateGuaranteeInformationHandler updateGuaranteeInformationHandler,
+            IssueGuaranteeHandler issueGuaranteeHandler,
+            ILogger<GuaranteesController> logger, 
+            TimeProvider timeProvider)
         {
             _createGuaranteeHandler = createGuaranteeHandler;
             _confirmGuaranteePriceHandler = confirmGuaranteePriceHandler;
+            _issueGuaranteeHandler = issueGuaranteeHandler;
+            _updateGuaranteeInformationHandler = updateGuaranteeInformationHandler;
             _logger = logger;
+            _timeProvider = timeProvider;
         }
 
         [HttpPost]
@@ -53,7 +66,59 @@ namespace EventSourcingPoc.API.Controllers
             await _confirmGuaranteePriceHandler.Handle(command, CancellationToken.None);
             return Ok();
         }
+
+        [HttpPost("{id}/issue")]
+        public async Task<ActionResult> IssueGuaranteeAsync(Guid id, CancellationToken cancellationToken)
+        {
+            IssueGuaranteeCommand command = new(id, _timeProvider.GetUtcNow().UtcDateTime);
+            await _issueGuaranteeHandler.Handle(command, cancellationToken);
+            return Ok();
+        }
+
+
+        /// <summary>
+        /// Patch guarantee information
+        /// </summary>
+        /// <param name="id">Guarantee ID</param>
+        /// <param name="payload">Payload with information to update</param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns></returns>
+        [HttpPatch("{id}/info")]
+        public async Task<ActionResult> PatchGuaranteeInformation(Guid id, PatchGuaranteeInfoPayload payload, CancellationToken cancellationToken)
+        {
+            UpdateGuaranteeInformationCommand updateGuaranteeInformationCommand = new(
+                Id: id,
+                Gloss: payload.Gloss,
+                TenderId: payload.TenderId,
+                SupplierInfo: payload.Supplier != null 
+                    ? new UpdateGuaranteePartyInformation(payload.Supplier.Name, payload.Supplier.AddressStreet, payload.Supplier.AddressLocation, payload.Supplier.AddressRegion) 
+                    :null,
+                BeneficiaryInfo: payload.Beneficiary != null
+                    ? new UpdateGuaranteePartyInformation(payload.Beneficiary.Name, payload.Beneficiary.AddressStreet, payload.Beneficiary.AddressLocation, payload.Beneficiary.AddressRegion)
+                    : null
+            );
+
+            var response = await _updateGuaranteeInformationHandler.Handle(updateGuaranteeInformationCommand, cancellationToken);
+            if (!response.Success)
+                return Problem(
+                    title: "Ha ocurrido un error al actualizar datos de la garantia",
+                    detail: response.Error,
+                    statusCode: StatusCodes.Status422UnprocessableEntity
+                );
+
+            return Ok(response.Value);
+        }
+
     }
+
+    public record PatchGuaranteeInfoPayload(
+        string? Gloss,
+        string? TenderId,
+        PatchGuaranteeLegalParty? Beneficiary,
+        PatchGuaranteeLegalParty? Supplier
+    );
+
+    public record PatchGuaranteeLegalParty(string? Name, string? AddressStreet, string? AddressLocation, string? AddressRegion);
 
     public record ConfirmGuaranteeCostPayload(decimal Cost);
 }
